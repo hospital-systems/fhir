0e9a1aa22b1cebb71424a9ae19fd80d66fcba1a6
dmitry-n <nechayevdv@gmail.com>
2013-12-11 15:42:37 +0400

    added fields version, primary, value_set to coding

--- lib/fhir/handmade/coding.rb
+++ lib/fhir/handmade/coding.rb
@@ -3,11 +3,20 @@ class Fhir::Coding < Fhir::DataType
   # Identity of the terminology system
   attribute :system, Fhir::URI
 
+  #The version of the code system
+  attribute :version, String
+
   # Symbol in syntax defined by the system
   attribute :code, Fhir::Code
 
   # Representation defined by the system
   attribute :display, String
+
+  #Indicates that this code was chosen by a user directly
+  attribute :primary, Boolean
+
+  #The set of possible coded values this coding was chosen from or constrained by.
+  attribute :value_set, Fhir::URI
 
   def initialize(raw_attrs)
     attrs = raw_attrs.dup
